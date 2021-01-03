30b926802d581fa2c75bef9a23c8f651b109a8ca
Maanav Seth <manuseth30@gmail.com>
2021-01-03 14:37:26 +0530

    initial commit

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./navbar.css";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="navbar">This is the navbar. write your code here.</div>
+    );
+  }
+}
+
+export default Navbar;
